0ca33b645dcdd1ecf9ed09b783330fe377dddd2e
Victor Chui <biscitplaymc@gmail.com>
2024-11-12 23:22:34 +0800

    feat: add SubscriberController and WebSocketConfig

---
+++ src/main/java/org/fyp24064/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package java.org.fyp24064.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/subscribe");
+        config.setApplicationDestinationPrefixes("/publish"); // Be used for all the messaging mappings
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/connect/chat");
+    }
+
+}
