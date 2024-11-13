0b517fb43253cb45b8c896a48f073e55f1944b29
Victor Chui <biscitplaymc@gmail.com>
2024-11-13 20:04:51 +0800

    feat: add redis configurations and chat room repository

--- src/main/java/org/fyp24064/controller/PublisherController.java
+++ src/main/java/org/fyp24064/controller/PublisherController.java
@@ -1,16 +1,45 @@
 package java.org.fyp24064.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.*;
 
+import java.org.fyp24064.model.ChatMessagePayload;
+import java.org.fyp24064.model.ChatRoom;
 import java.org.fyp24064.model.dto.CreateChatRoomDTO;
+import java.org.fyp24064.repository.ChatRoomRepository;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/chat")
 public class PublisherController {
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
     @PostMapping(path = "/")
-    public String createChatRoom(@RequestBody CreateChatRoomDTO chatRoomDTO) {
-        // TODO: Add logic to create chat room
+    public ChatRoom createChatRoom(@RequestBody CreateChatRoomDTO chatRoomDTO) {
         // TODO: Create entities for chat rooms
-        return "";
+        ChatRoom chatRoom = ChatRoom.getBuilder()
+                .setRoomTitle(chatRoomDTO.getRoomTitle())
+                .setId(UUID.randomUUID())
+                .setLastMessage("")
+                .setMembers(chatRoomDTO.getMembers())
+                .setMessages(new ArrayList<>())
+                .build();
+        return chatRoomRepository.save(chatRoom);
+    }
+
+    @MessageMapping(value = "/chat/message")
+    public ResponseEntity<?> sendMessage(ChatMessagePayload messagePayload) {
+        String chatRoomId = messagePayload.getRoomId();
+        Optional<ChatRoom> chatRoom = chatRoomRepository.findById(chatRoomId);
+        if (chatRoom.isEmpty()) {
+            return new ResponseEntity<>("Unable to send", HttpStatus.BAD_REQUEST);
+        }
+        // TODO: Add RedisPublisher to publish the message in this
+        return new ResponseEntity<>("Sent", HttpStatus.ACCEPTED);
     }
 }
--- src/main/java/org/fyp24064/controller/SubscriberController.java
+++ src/main/java/org/fyp24064/controller/SubscriberController.java
@@ -4,18 +4,28 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.org.fyp24064.model.ChatRoom;
 import java.org.fyp24064.repository.ChatMessageRepository;
+import java.org.fyp24064.repository.ChatRoomRepository;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/chat")
 public class SubscriberController {
     @Autowired
     private ChatMessageRepository chatMessageRepository;
+
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
+
+    @GetMapping(path = "/{userId}/chatRoom")
+    public List<ChatRoom> getChatRoomForUser(@PathVariable("userId") String userId) {
+        // TODO: Add findAllByUserId method, add implementation
+        return chatRoomRepository.findAllById(userId);
+    }
 
     @GetMapping(path = "/{roomId}/messages")
     public ResponseEntity<?> getChatMessages(@PathVariable("roomId") Long roomId) {
